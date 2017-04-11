be30e54fc6fd443a2511d54127f10f3b5d236b02
Kiryl Lutsyk <zetsubou.zero.0@gmail.com>
2017-04-11 23:58:36 +0300

    session 1

--- java-lazy/src/main/java/session01/MethodInvestigation.java
+++ java-lazy/src/main/java/session01/MethodInvestigation.java
@@ -1,27 +1,42 @@
 package main.java.session01;
 
 public class MethodInvestigation {
+    String name; // null
+    int age = 12; // 0
 
     public static void main(String[] args) {
         String nyanko;
         printHelloWorld();
         nyanko = "neko";
         printHello(nyanko);
+        String res = nameAndSquare("chinko", 3);
+        System.out.println(res);
+
+        String x;
+        x = "world " + 25;
+        printHello(x);
+
+        printHello(nameAndSquare("world", 5));
+
     }
 
-    public static void printHelloWorld() {
+    static void printHelloWorld() {
         System.out.print("Hello");
         System.out.print(" ");
         System.out.println("world");
     }
 
-    public static void printHello(String name) {
+    static void printHello(String name) {
         System.out.print("Hello");
         System.out.print(" ");
         System.out.println(name);
     }
 
-    public static String nameAndSquare() {
+    static String nameAndSquare(String word, int number) {
+        return word + " " + (number * number);
+    }
 
+    void printInfo() {
+        System.out.println(age);
     }
 }
