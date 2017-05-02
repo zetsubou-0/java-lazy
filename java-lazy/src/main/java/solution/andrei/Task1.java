ef550c5f35c9777c803aeba3eedafd49f085fd7c
Andrei <demon_19888888@mail.ru>
2017-05-02 15:33:30 +0300

    task1  1.2.3.4 пункт

--- java-lazy/src/main/java/solution/andrei/Task1.java
+++ java-lazy/src/main/java/solution/andrei/Task1.java
@@ -1,7 +1,50 @@
 package solution.andrei;
 
+/*1. Создать любой класс с методом main
+
+2. В этом методе создать переменные следующих типов:
+
+3. int, long, float, double, boolean, char, String. При чем переменная типа double должна быть инициализированна значением 0б в последствии присвоено значени;
+а переменная типа String инициализированна null, в последствии задано значение; переменная типа char объявлена без инициализации, в последствии задано значение.
+
+4. Создать другие переменные типов int, long, float, double, при чем в каждую необходимо сохранить по не сколько значений разных типов (1, 2, 3, 4 соотвтетственно).
+К примеру:
+long myVar = 4;
+myVar = 5L;*/
 public class Task1 {
     public static void main (String[] args){
+        int i;
+        long l;
+        float f;
+        double d = 0;
+        d = 3.14;
+        boolean b;
+        char c;
+        c = 65;
+        String s = null;
+        s = "Andrey";
+
+        int a = 1;
+        long la = (long)a;
+        float fa = (float)a;
+        double da = (double)a;
+
+        long x = 1L;
+        int ix = (int)x;
+        float fx = (float)x;
+        double dx = (double)x;
+
+        float v = 45.545f;
+        int iv = (int)v;
+        long lv = (long)v;
+        double dv = (double)v;
+
+        double n = 3.14;
+        int in = (int)n;
+        long ln = (long)n;
+        float fn = (float)n;
+
+
 
     }
 }
