f78823ab98f3db494f7d4062a07f10e0e29334f9
Andrei <demon_19888888@mail.ru>
2017-05-02 19:36:06 +0300

    task2  1-3 пункт

--- java-lazy/src/main/java/solution/andrei/Task2.java
+++ java-lazy/src/main/java/solution/andrei/Task2.java
@@ -5,6 +5,8 @@ public class Task2 {
         System.out.println(translation(number));
         int value = 5;
         System.out.print(answer(value));
+        System.out.print(mood(value));
+
     }
     private static double translation (double number){
         return number/100;
@@ -14,6 +16,16 @@ public class Task2 {
             return true;
         }else {
             return false;
+        }
+    }
+    private static String mood (long value ){
+        if (value > 0){
+            return "positive";
+        }
+        if (value < 0){
+            return "negative";
+        }else {
+            return "zero";
         }
     }
 }
