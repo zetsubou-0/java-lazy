209c3eefaaeef14738de028292c9ee1c12263781
Andrei <demon_19888888@mail.ru>
2017-05-02 19:25:54 +0300

    task2  1-2 пункт

--- java-lazy/src/main/java/solution/andrei/Task2.java
+++ java-lazy/src/main/java/solution/andrei/Task2.java
@@ -2,9 +2,18 @@ package solution.andrei;
 public class Task2 {
     public static void main (String[] args){
         double number = 12.5;
-        System.out.print(translation(number));
+        System.out.println(translation(number));
+        int value = 5;
+        System.out.print(answer(value));
     }
     private static double translation (double number){
         return number/100;
+    }
+    private static boolean answer (int value){
+        if (value>=0){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
