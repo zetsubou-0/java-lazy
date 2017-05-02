215af209c461c00973d76a2c679377426878c723
Andrei <demon_19888888@mail.ru>
2017-05-02 17:36:07 +0300

    task2  1 пункт

--- java-lazy/src/main/java/solution/andrei/Task2.java
+++ java-lazy/src/main/java/solution/andrei/Task2.java
@@ -1,10 +1,10 @@
 package solution.andrei;
 public class Task2 {
     private static void main (String[] args){
-        double figure ;
-    }
-    private double translation (double figure){
+        double figure = 12.5;
         System.out.println (figure/100);
+    }
+    private static double translation (double figure){
         return figure/100;
     }
 }
