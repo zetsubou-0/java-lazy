a7c35c7527a6a2f4c3d3a6f9922509940d21fb4a
Andrei <demon_19888888@mail.ru>
2017-11-04 16:53:35 +0200

    Task 3.1

--- java-lazy/src/main/java/solution/andrei/Task3.java
+++ java-lazy/src/main/java/solution/andrei/Task3.java
@@ -3,13 +3,12 @@ package solution.andrei;
 //1. Создать метод, который выводит в консоль переданный double в формате "два символа после запятой"(использовать форматирование строки)
 
 public class Task3 {
-    public static void main(String[] args){
-        double number =3.466;
-        System.out.printf( "%.2f",deduces(number));
-        System.out.println( );
-        System.out.println( );
+    public static void main(String[] args) {
+        double number = 3.466;
+        deduces(number);
     }
-    public static double deduces(double a) {
-     return a;
+
+    public static void deduces(double a) {
+        System.out.printf("%.2f%n", a);
     }
 }
