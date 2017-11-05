d6d9c87d82144311ae7b348d0fae169c7d6e0cb2
Andrei <demon_19888888@mail.ru>
2017-11-05 16:48:45 +0200

    Task 3.2

--- java-lazy/src/main/java/solution/andrei/Task3.java
+++ java-lazy/src/main/java/solution/andrei/Task3.java
@@ -8,12 +8,14 @@ public class Task3 {
         deduces(number);
 
         String firstLine = "Валиииик";
-        String secondLine = "Кииряяя";
+        String secondLine = "Валииик";
         comparison(firstLine,secondLine);
+
+
     }
     //1. Создать метод, который выводит в консоль переданный double в формате "два символа после запятой"(использовать форматирование строки)
     private static void deduces(double a) {
-        System.out.printf("%.2f%n", a);
+        System.out.printf("%.2f%n%n", a);
     }
     /*2. Создать метод, принимающий в качестве параметров две строки и выводящее в консоль
     "Строки идентичны" если они одинаковые,
@@ -22,17 +24,16 @@ public class Task3 {
     иначе "Строки разные"*/
 
     private static void comparison(String firstline,String secondline){
-        if(firstline == secondline){
+        if(firstline.equals(secondline)){
             System.out.println("Строки идентичны");
-        }else if (firstline.length() == secondline.length()){
-            System.out.println("Длины строк равны");
-            if (firstline.indexOf(firstline) == secondline.indexOf(secondline)){
-                System.out.println("Совпадает первый символ");
-            }
-        }else {
+        } else {
             System.out.println("Строки разные");
         }
-
+        if (firstline.length() == secondline.length()) {
+            System.out.println("Длины строк равны");
+        }
+        if (firstline.charAt(0) == secondline.charAt(0)){
+            System.out.println("Совпадает первый символ");
+        }
     }
-
 }
