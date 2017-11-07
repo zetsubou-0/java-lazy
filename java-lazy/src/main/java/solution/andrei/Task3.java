3979b0cfe13278bbbe35eab7a2a99390a3a6199e
Andrei <demon_19888888@mail.ru>
2017-11-07 21:25:09 +0300

    Task 3.4

--- java-lazy/src/main/java/solution/andrei/Task3.java
+++ java-lazy/src/main/java/solution/andrei/Task3.java
@@ -14,6 +14,8 @@ public class Task3 {
         //3
         int SecondNumber = 0;
         defineNumber(SecondNumber);
+        //4
+        square ();
     }
     //1. Создать метод, который выводит в консоль переданный double в формате "два символа после запятой"(использовать форматирование строки)
     private static void deduces(double a) {
@@ -27,15 +29,15 @@ public class Task3 {
 
     private static void comparison(String firstline,String secondline){
         if(firstline.equals(secondline)){
-            System.out.println("Строки идентичны");
+            System.out.printf("%s%n%n","Строки идентичны");
         } else {
-            System.out.println("Строки разные");
+            System.out.printf("%s%n%n","Строки разные");
         }
         if (firstline.length() == secondline.length()) {
-            System.out.println("Длины строк равны");
+            System.out.printf("%s%n%n","Длины строк равны");
         }
         if (firstline.charAt(0) == secondline.charAt(0)){
-            System.out.println("Совпадает первый символ");
+            System.out.printf("%s%n%n","Совпадает первый символ");
         }
     }
 
@@ -53,15 +55,27 @@ public class Task3 {
 
     private static void defineNumber (int number){
         if (number < 0 && number % 2 == 0){
-            System.out.println("отрицательное четное число");
+            System.out.printf("%s%n%n","отрицательное четное число");
         }else if (number < 0 && number % 2 == -1){
-            System.out.println("отрицательное нечетное число");
+            System.out.printf("%s%n%n","отрицательное нечетное число");
         }else if (number > 0 && number % 2 == 0){
-            System.out.println("положительное четное число");
+            System.out.printf("%s%n%n","положительное четное число");
         }else if (number > 0 && number % 2 == 1){
-            System.out.println("положительное нечетное число");
+            System.out.printf("%s%n%n","положительное нечетное число");
         }else{
-            System.out.println("нулевое число");
+            System.out.printf("%s%n%n","нулевое число");
         }
     }
+    //4. Создать метод выводящий на экран квадрат из 10х10 букв S используя цикл while. Буквы в каждой строке не разделять.
+
+    private static void square (){
+        String letter = "SSSSSSSSSS";
+        int max = 10;
+        int i = 1;
+        while (i <= max){
+            System.out.printf("%.10s%n",letter);
+            i++;
+        }
+    }
+
 }
