4178976b24b2109d8dddf5eb75bde51125f1d190
VADIM <git_on@mail.ru>
2018-03-12 00:54:22 +0300

    Task01_(1, 2 of 7)

---
+++ java-lazy/src/main/java/solution/vadim/course/Task01.java
@@ -0,0 +1,25 @@
+package solution.vadim.course;
+
+
+public class Task01 {
+    public static void main(String[] args) {
+        int intVar;
+        long longVar;
+        byte byteVar;
+        char charVar;
+        double doubleVar;
+        float floatVar;
+        boolean booleanVar;
+        short shortVar;
+
+        intVar = 8;
+        longVar = 10L;
+        byteVar = 0b100;  // для наглядности
+        charVar = 'Z';
+        doubleVar = 0.0;
+        floatVar = 5.0F;
+        booleanVar = true;
+        shortVar = 0;
+    }
+
+}
