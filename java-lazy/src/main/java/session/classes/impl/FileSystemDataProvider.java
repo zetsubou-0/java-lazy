65f7a3117ba634d3e46b763d9c9490e62daa5cdf
k.lutsyk <Kiryl.Lutsyk@6dglobal.com>
2018-09-04 22:57:22 +0300

    fs data provider

---
+++ java-lazy/src/main/java/session/classes/impl/FileSystemDataProvider.java
@@ -0,0 +1,17 @@
+package session.classes.impl;
+
+import session.classes.Data;
+import session.classes.DataProvider;
+
+public class FileSystemDataProvider implements DataProvider {
+
+    @Override
+    public void save(String name, Data info) {
+
+    }
+
+    @Override
+    public Data read(String name) {
+        return null;
+    }
+}
