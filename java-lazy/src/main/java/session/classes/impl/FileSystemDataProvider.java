26fcc0d288b683ea986bfbd560bfade98e353222
k.lutsyk <Kiryl.Lutsyk@6dglobal.com>
2018-09-07 00:01:53 +0300

    array increase

--- java-lazy/src/main/java/session/classes/impl/FileSystemDataProvider.java
+++ java-lazy/src/main/java/session/classes/impl/FileSystemDataProvider.java
@@ -12,8 +12,8 @@ public class FileSystemDataProvider implements DataProvider {
     public void save(String name, Data info) {
         for (int i = 0; i < paths.length; i++) {
             if (paths[i] == null) {
-                paths[i] = name;
-                data[i] = info;
+                addPath(i, name);
+                addData(i, info);
             }
             if (name.equals(paths[i])) {
                 return;
@@ -29,5 +29,44 @@ public class FileSystemDataProvider implements DataProvider {
             }
         }
         return null;
+    }
+
+    /**
+     * Метод ложит элемент (path) в массив на место index
+     * Внутренняя реализация: если массив достиг максимального размера, он увиличивается в 2 раза
+     * @param index индекс элемента, куда ты хочешь положить данные
+     * @param path путь (элемент)
+     */
+    public void addPath(int index, String path) {
+//        int count = 10;
+//        String[] paths = new String[count];
+//        for (int i = 0; i < paths.length; i++) {
+//            if (i < paths.length) {
+//                paths[index] = path;
+//            }
+//            if (i == paths.length - 1) {
+//                String[] newPaths = new String[count * 2];
+//                newPaths = paths;
+//            }
+//        }
+
+
+
+    }
+
+    public void addData(int index, Data data) {
+
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        FileSystemDataProvider dataProvider = new FileSystemDataProvider();
+        dataProvider.paths = new String[10];
+
+        for (int i = 0; i < 55; i++) {
+            dataProvider.addPath(i, "Path - " + (i + 1));
+        }
+        for (String path : dataProvider.paths) {
+            System.out.println(path);
+        }
     }
 }
