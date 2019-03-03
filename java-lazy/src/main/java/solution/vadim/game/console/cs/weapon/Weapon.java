c2ede2ca6c89b0ecc9c46339ddca89f6362656d7
k.lutsyk <Kiryl.Lutsyk@6dglobal.com>
2019-03-03 12:41:43 +0300

    Vadim's code have been fixed
     - Класс с большой буквы
     - Не заливать нерабочий код !!!!

---
+++ java-lazy/src/main/java/solution/vadim/game/console/cs/weapon/Weapon.java
@@ -0,0 +1,17 @@
+package solution.vadim.game.console.cs.weapon;
+
+public class Weapon {
+    /**
+     * Класс с большой буквы
+     * Не заливать нерабочий код !!!!
+     */
+//    protected final int cost;
+//    protected final int damage;
+//    protected final String team;
+
+
+    Weapon(){
+
+    }
+}
+
