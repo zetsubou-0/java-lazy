f8a3ca73e1e49a2278727c98ed597211ad0f6c78
Andrei <demon_19888888@mail.ru>
2019-04-01 20:26:08 +0300

    6 готов

--- java-lazy/src/main/java/solution/andrei/city/equipment/factory/ElectricEquipmentFactory.java
+++ java-lazy/src/main/java/solution/andrei/city/equipment/factory/ElectricEquipmentFactory.java
@@ -1,10 +1,8 @@
 package solution.andrei.city.equipment.factory;
 
-import solution.andrei.city.equipment.factory.data.impl.RandomDataProvider;
 import solution.andrei.city.equipment.factory.impl.HeavyEquipmentFactory;
-import solution.andrei.city.equipment.factory.impl.PortableEquipmentFactory;
-import solution.andrei.city.equipment.factory.impl.RegularEquipmentFactory;
 import solution.andrei.city.equipment.model.ElectricEquipment;
+import solution.andrei.city.equipment.model.EquipmentParameter;
 import solution.andrei.city.equipment.model.EquipmentType;
 
 import java.util.*;
@@ -12,26 +10,21 @@ import java.util.*;
 public final class ElectricEquipmentFactory {
     private ElectricEquipmentFactory() {}
 
-    public static List<ElectricEquipment> create(EquipmentType type) {
+    public static List<ElectricEquipment> create(List<EquipmentParameter> parameters) {
         List<ElectricEquipment> listEquipment = new ArrayList<>();
-        RandomDataProvider randomDataProvider = new RandomDataProvider();
         Set<ElectricEquipment> electricEquipments;
-        switch (type) {
-            case HEAVY:
-                HeavyEquipmentFactory heavyEquipmentFactory = new HeavyEquipmentFactory();
-                electricEquipments = heavyEquipmentFactory.create(randomDataProvider.generateParameters(type));
-                listEquipment.addAll(electricEquipments);
-                break;
-            case PORTABLE:
-                PortableEquipmentFactory portableEquipmentFactory = new PortableEquipmentFactory();
-                electricEquipments = portableEquipmentFactory.create(randomDataProvider.generateParameters(type));
-                listEquipment.addAll(electricEquipments);
-                break;
-            case REGULAR:
-                RegularEquipmentFactory regularEquipmentFactory = new RegularEquipmentFactory();
-                electricEquipments = regularEquipmentFactory.create(randomDataProvider.generateParameters(type));
-                listEquipment.addAll(electricEquipments);
-                break;
+        if (parameters.get(0).getEquipmentType().equals(EquipmentType.HEAVY)) {
+            HeavyEquipmentFactory heavyEquipmentFactory = new HeavyEquipmentFactory();
+            electricEquipments = heavyEquipmentFactory.create(parameters);
+            listEquipment.addAll(electricEquipments);
+        }else if (parameters.get(0).getEquipmentType().equals(EquipmentType.PORTABLE)) {
+            HeavyEquipmentFactory heavyEquipmentFactory = new HeavyEquipmentFactory();
+            electricEquipments = heavyEquipmentFactory.create(parameters);
+            listEquipment.addAll(electricEquipments);
+        }else if (parameters.get(0).getEquipmentType().equals(EquipmentType.REGULAR)) {
+            HeavyEquipmentFactory heavyEquipmentFactory = new HeavyEquipmentFactory();
+            electricEquipments = heavyEquipmentFactory.create(parameters);
+            listEquipment.addAll(electricEquipments);
         }
         return listEquipment;
     }
