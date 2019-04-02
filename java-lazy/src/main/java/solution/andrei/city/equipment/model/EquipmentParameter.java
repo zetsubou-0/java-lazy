28ede8bf6a80dba74293af05570dd26177d748f1
AAAndrei <demon_19888888@mail.ru>
2019-04-02 15:49:45 +0200

    7 пункт попытки совершить невозможное!

--- java-lazy/src/main/java/solution/andrei/city/equipment/model/EquipmentParameter.java
+++ java-lazy/src/main/java/solution/andrei/city/equipment/model/EquipmentParameter.java
@@ -1,5 +1,7 @@
 package solution.andrei.city.equipment.model;
 
+import solution.kiryl.city.equipment.model.ElectricEquipment;
+
 public class EquipmentParameter {
     private final EquipmentType equipmentType;
     private final int power;
@@ -108,5 +110,74 @@ public class EquipmentParameter {
                 ", resolutionOnWidth=" + resolutionWidth +
                 ", resolutionOnHeight=" + resolutionHeight +
                 '}';
+    }
+    private class EquipmentParameterBuilder {
+        private final EquipmentType equipmentType;
+        private final int power;
+        private final int price;
+        private double capacity;
+        private int width, height, length;
+        private double screenSize;
+        private int batteryCapacity;
+        private int resolutionWidth;
+        private int resolutionHeight;
+
+        private EquipmentParameterBuilder(EquipmentType equipmentType, int power, int price) {
+            this.equipmentType = equipmentType;
+            this.power = power;
+            this.price = price;
+        }
+
+        private EquipmentParameterBuilder appendCapacity(double capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendHeight(int height) {
+            this.height = height;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendLength(int length) {
+            this.length = length;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendScreenSize(double screenSize) {
+            this.screenSize = screenSize;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendBatteryCapacity(int batteryCapacity) {
+            this.batteryCapacity = batteryCapacity;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendResolutionWidth(int resolutionWidth) {
+            this.resolutionWidth = resolutionWidth;
+            return this;
+        }
+
+        private EquipmentParameterBuilder appendResolutionHeight(int resolutionHeight) {
+            this.resolutionHeight = resolutionHeight;
+            return this;
+        }
+        private EquipmentParameter bulid() {
+            EquipmentParameter equipmentParameter = new EquipmentParameter(equipmentType, power, price);
+            equipmentParameter.setCapacity(capacity);
+            equipmentParameter.setWidth(width);
+            equipmentParameter.setHeight(height);
+            equipmentParameter.setLength(length);
+            equipmentParameter.setScreenSize(screenSize);
+            equipmentParameter.setBatteryCapacity(batteryCapacity);
+            equipmentParameter.setResolutionWidth(resolutionWidth);
+            equipmentParameter.setResolutionHeight(resolutionHeight);
+            return equipmentParameter;
+        }
     }
 }
