22e42ecf060e5926cfc2ef585718f8eeb2ea78b6
Kiryl Ltsyk <zetsubou.zero.0@gmail.com>
2019-04-23 14:54:45 +0300

    Task 3, point 8, part 4 условие обновлено - лишняя проверка убрана

--- java-lazy/src/test/java/solition/andrei/city/PartFilledApartmentListTest.java
+++ java-lazy/src/test/java/solition/andrei/city/PartFilledApartmentListTest.java
@@ -35,7 +35,7 @@ public class PartFilledApartmentListTest {
 
     private void checkCollectionCreation(double percent, int expectedSize) {
         sut = new PartFilledApartmentList(MAX_SIZE, percent);
-        assertEquals("При создании коллекции размер должен быть нулевым", 0, sut.size());
+        assertEquals(0, sut.size());
         fillWithData();
         checkCollectionSize(expectedSize, sut.size());
     }
@@ -45,9 +45,7 @@ public class PartFilledApartmentListTest {
     }
 
     private void checkCollectionSize(int expectedSize, long size) {
-        assertEquals("Неверный размер заполненой коллекции. Должен быть: " + expectedSize +
-                        ", реальный: " + size
-                , expectedSize, size);
+        assertEquals(expectedSize, size);
     }
 
     private long getFilledCount() {
@@ -63,14 +61,14 @@ public class PartFilledApartmentListTest {
 
     @Test
     public void shouldAddElement() {
-        assertTrue("Элемент должен быть добавлен в коллекцию", sut.add(TEST_APARTMENTS.get(0)));
+        assertTrue(sut.add(TEST_APARTMENTS.get(0)));
     }
 
     @Test
     public void shouldNotAddElement() {
         createSingleElementCollection();
         sut.add(TEST_APARTMENTS.get(0));
-        assertFalse("Элемент не должен быть добавлен в коллекцию", sut.add(TEST_APARTMENTS.get(1)));
+        assertFalse(sut.add(TEST_APARTMENTS.get(1)));
     }
 
     @Test
@@ -87,48 +85,48 @@ public class PartFilledApartmentListTest {
     @Test
     public void shouldFindElement() {
         fillWithData();
-        assertTrue("Элемент из заполненйо коллекции должен быть найден", sut.contains(TEST_APARTMENTS.get(5)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(5)));
     }
 
     @Test
     public void shouldNotFindElement() {
         fillWithData();
-        assertFalse("Элемент из заполненйо коллекции не должен быть найден, должен отсутствовать в ней", sut.contains(TEST_APARTMENTS.get(6)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(6)));
     }
 
     @Test
     public void shouldFindNullElementInPartFilledCollection() {
         fillWithData();
-        assertTrue("Должен найти null элемент в заполненой коллекции", sut.contains(null));
+        assertTrue(sut.contains(null));
     }
 
     @Test
     public void shouldNotFindNullInEmptyCollection() {
-        assertFalse("Не должен найти null элемент в пустой коллеции", sut.contains(null));
+        assertFalse(sut.contains(null));
     }
 
     @Test
     public void shouldNotFindNullInFullCollection() {
         createFullLoadedList();
         fillWithData();
-        assertFalse("Не должен найти null элемент в пустой коллеции", sut.contains(null));
+        assertFalse(sut.contains(null));
     }
 
     @Test
     public void shouldFindAllElements() {
         fillWithData();
-        assertTrue("Должно найти все элементы из заполняемой коллекции, которые влазат в нее", sut.containsAll(EXISTED_APARTMENTS));
+        assertTrue(sut.containsAll(EXISTED_APARTMENTS));
     }
 
     @Test
     public void shouldNotFindNullCollectionInEmptyCollection() {
-        assertFalse("Должно не найти null элементы пустой коллекции", sut.containsAll(Collections.singletonList(null)));
+        assertFalse(sut.containsAll(Collections.singletonList(null)));
     }
 
     @Test
     public void shouldFindNullInNotEmptyCollection() {
         fillWithData();
-        assertTrue("Должно найти null элементы в заполняемой частично коллекции (в которой они есть)", sut.containsAll(Collections.singletonList(null)));
+        assertTrue(sut.containsAll(Collections.singletonList(null)));
     }
 
     @Test
@@ -140,14 +138,14 @@ public class PartFilledApartmentListTest {
         final int testIndex = 4;
         createFullLoadedList();
         fillWithData();
-        assertEquals("Должен найти элемент по индексу", TEST_APARTMENTS.get(testIndex), sut.get(testIndex));
+        assertEquals(TEST_APARTMENTS.get(testIndex), sut.get(testIndex));
     }
 
     @Test
     public void shouldReturnRightEmptyStatus() {
-        assertTrue("Коллекция должна быть пустой", sut.isEmpty());
+        assertTrue(sut.isEmpty());
         fillWithData();
-        assertFalse("Коллекция должна быть заполнена", sut.isEmpty());
+        assertFalse(sut.isEmpty());
     }
 
     @Test
@@ -167,14 +165,14 @@ public class PartFilledApartmentListTest {
         final int expectedSize = 1;
 
         sut.add(testIndex, TEST_APARTMENTS.get(testIndex));
-        assertEquals("Должно добавить данные в случайно выбранную свободную ячейку (неверный размер)", expectedSize, sut.size());
-        assertEquals("Должно добавить данные в случайно выбранную свободную ячейку (неверные данные)", TEST_APARTMENTS.get(testIndex), sut.get(testIndex));
+        assertEquals(expectedSize, sut.size());
+        assertEquals(TEST_APARTMENTS.get(testIndex), sut.get(testIndex));
     }
 
     @Test
     public void shouldAddAllElements() {
-        assertTrue("Должен добавить все элементы", sut.addAll(EXISTED_APARTMENTS));
-        assertTrue("Должен добавить все элементы", sut.containsAll(EXISTED_APARTMENTS));
+        assertTrue(sut.addAll(EXISTED_APARTMENTS));
+        assertTrue(sut.containsAll(EXISTED_APARTMENTS));
     }
 
     @Test(expected = UnsupportedOperationException.class)
@@ -199,7 +197,7 @@ public class PartFilledApartmentListTest {
     @Test
     public void shouldGetIndexOf() {
         createFullLoadedList();
-        assertEquals("Должно найти индекс элемета", 0, sut.indexOf(null));
+        assertEquals(0, sut.indexOf(null));
 
         sut.add(TEST_APARTMENTS.get(10));
         sut.add(TEST_APARTMENTS.get(1));
@@ -209,13 +207,13 @@ public class PartFilledApartmentListTest {
         sut.add(TEST_APARTMENTS.get(4));
         sut.add(TEST_APARTMENTS.get(3));
         sut.add(TEST_APARTMENTS.get(2));
-        assertEquals("Должно найти индекс элемета", 4, sut.indexOf(TEST_APARTMENTS.get(4)));
+        assertEquals(4, sut.indexOf(TEST_APARTMENTS.get(4)));
     }
 
     @Test
     public void shouldGetLastElementByIndex() {
         createFullLoadedList();
-        assertEquals("Должно найти индекс элемета", MAX_SIZE - 1, sut.lastIndexOf(null));
+        assertEquals(MAX_SIZE - 1, sut.lastIndexOf(null));
 
         sut.add(TEST_APARTMENTS.get(10));
         sut.add(TEST_APARTMENTS.get(1));
@@ -225,19 +223,16 @@ public class PartFilledApartmentListTest {
         sut.add(TEST_APARTMENTS.get(4));
         sut.add(TEST_APARTMENTS.get(2));
         sut.add(TEST_APARTMENTS.get(3));
-        assertEquals("Должно найти индекс элемета", 5, sut.lastIndexOf(TEST_APARTMENTS.get(4)));
+        assertEquals(5, sut.lastIndexOf(TEST_APARTMENTS.get(4)));
     }
 
     @Test
     public void shouldReturnSublist() {
-        assertEquals("Размер саблиста должен совпадать", 5, sut.subList(5, 10).size());
+        assertEquals(5, sut.subList(5, 10).size());
 
         createFullLoadedList();
         fillWithData();
-        assertEquals(
-                "Саблисты должны совпадать",
-                ImmutableList.of(TEST_APARTMENTS.get(3), TEST_APARTMENTS.get(4), TEST_APARTMENTS.get(5)),
-                sut.subList(3, 6));
+        assertEquals(ImmutableList.of(TEST_APARTMENTS.get(3), TEST_APARTMENTS.get(4), TEST_APARTMENTS.get(5)), sut.subList(3, 6));
     }
 
     @Test
@@ -250,40 +245,40 @@ public class PartFilledApartmentListTest {
     @Test
     public void shouldRemoveElement() {
         fillWithData();
-        assertTrue("Элемент должен быть удален из коллекции", sut.remove(TEST_APARTMENTS.get(3)));
-        assertFalse("Элемент должен быть удален из коллекции", sut.contains(TEST_APARTMENTS.get(3)));
+        assertTrue(sut.remove(TEST_APARTMENTS.get(3)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(3)));
     }
 
     @Test
     public void shouldNotRemoveNullElement() {
         fillWithData();
-        assertFalse(" null элемент не должен быть удален из коллекции", sut.remove(null));
+        assertFalse(sut.remove(null));
     }
 
     @Test
     public void shouldRemoveElementByIndex() {
         createFullLoadedList();
         fillWithData();
-        assertEquals("Элемент должен быть удален из коллекции", TEST_APARTMENTS.get(3), sut.remove(3));
-        assertFalse("Элемент должен быть удален из коллекции", sut.contains(TEST_APARTMENTS.get(3)));
+        assertEquals(TEST_APARTMENTS.get(3), sut.remove(3));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(3)));
     }
 
     @Test
     public void shouldRemoveNotRemoveNullElementByIndex() {
         createFullLoadedList();
-        assertNull("Элемент null не должен быть удален из коллекции", sut.remove(3));
+        assertNull(sut.remove(3));
     }
 
     @Test
     public void shouldConvertToObjectsArray() {
         fillWithData();
         final Object[] array = sut.toArray();
-        assertEquals("Неверный размер массива", MAX_SIZE, array.length);
+        assertEquals(MAX_SIZE, array.length);
         Arrays.stream(array)
                 .filter(Objects::nonNull)
                 .filter(Apartment.class::isInstance)
                 .map(Apartment.class::cast)
-                .forEach(val -> assertTrue("Элемент массива должен содержаться в исходной коллекции", TEST_APARTMENTS.contains(val)));
+                .forEach(val -> assertTrue(TEST_APARTMENTS.contains(val)));
     }
 
     @Test
@@ -291,10 +286,10 @@ public class PartFilledApartmentListTest {
         fillWithData();
         Apartment[] array = new Apartment[MAX_SIZE];
         array = sut.toArray(array);
-        assertEquals("Неверный размер массива", MAX_SIZE, array.length);
+        assertEquals(MAX_SIZE, array.length);
         Arrays.stream(array)
                 .filter(Objects::nonNull)
-                .forEach(val -> assertTrue("Элемент массива должен содержаться в исходной коллекции", TEST_APARTMENTS.contains(val)));
+                .forEach(val -> assertTrue(TEST_APARTMENTS.contains(val)));
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -306,14 +301,14 @@ public class PartFilledApartmentListTest {
 
     @Test
     public void shouldAddApartmentInEmptyPoint() {
-        assertTrue("Апартаменты должны добавиться если есть свободное место (null)", ((PartFilledApartmentList) sut).addApartment(3, TEST_APARTMENTS.get(3)));
+        assertTrue(((PartFilledApartmentList) sut).addApartment(3, TEST_APARTMENTS.get(3)));
     }
 
     @Test
     public void shouldNotAddApartmentInFilledPoint() {
         createFullLoadedList();
         fillWithData();
-        assertFalse("Апартаменты не должны добавиться если есть нет свободного места (null)", ((PartFilledApartmentList) sut).addApartment(3, TEST_APARTMENTS.get(3)));
+        assertFalse(((PartFilledApartmentList) sut).addApartment(3, TEST_APARTMENTS.get(3)));
     }
 
     @Test
@@ -335,14 +330,14 @@ public class PartFilledApartmentListTest {
     @Test
     public void shouldReturnIterator() {
         final Iterator<Apartment> apartmentIterator = sut.iterator();
-        assertNotNull("Итератор не должен быть null", apartmentIterator);
+        assertNotNull(apartmentIterator);
     }
 
     @Test
     public void shouldFindNext() {
         fillWithData();
         final Iterator<Apartment> apartmentIterator = sut.iterator();
-        assertTrue("Должен быть доступен следущий элемент в итераторе", apartmentIterator.hasNext());
+        assertTrue(apartmentIterator.hasNext());
     }
 
     @Test
@@ -351,7 +346,7 @@ public class PartFilledApartmentListTest {
         final Iterator<Apartment> apartmentIterator = sut.iterator();
         if (apartmentIterator.hasNext()) {
             apartmentIterator.next();
-            assertFalse("Должен быть не доступен следущий элемент в итераторе", apartmentIterator.hasNext());
+            assertFalse(apartmentIterator.hasNext());
             return;
         }
         throw new RuntimeException("Итератор не имеет следущего элемента, хотя должен");
@@ -362,7 +357,7 @@ public class PartFilledApartmentListTest {
         createSingleElementCollection();
         final Iterator<Apartment> apartmentIterator = sut.iterator();
         if (apartmentIterator.hasNext()) {
-            assertEquals("Должен быть доступен следущий элемент в итераторе", apartmentIterator.next(), TEST_APARTMENTS.get(0));
+            assertEquals(apartmentIterator.next(), TEST_APARTMENTS.get(0));
             return;
         }
         throw new RuntimeException("Итератор не имеет следущего элемента, хотя должен");
@@ -383,14 +378,14 @@ public class PartFilledApartmentListTest {
     @Test
     public void shouldReturnListIterator() {
         final ListIterator<Apartment> apartmentIterator = sut.listIterator();
-        assertNotNull("Итератор не должен быть null", apartmentIterator);
+        assertNotNull(apartmentIterator);
     }
 
     @Test
     public void shouldReturnListIteratorStartedWithIndex() {
         fillWithData();
         final ListIterator<Apartment> apartmentIterator = sut.listIterator(3);
-        assertNotNull("Итератор не должен быть null", apartmentIterator);
+        assertNotNull(apartmentIterator);
     }
 
     @Test
@@ -399,7 +394,7 @@ public class PartFilledApartmentListTest {
         fillWithData();
         final ListIterator<Apartment> apartmentListIterator = sut.listIterator();
         apartmentListIterator.next();
-        assertTrue("Должен найти предыдущий элемент", apartmentListIterator.hasPrevious());
+        assertTrue(apartmentListIterator.hasPrevious());
     }
 
     @Test
@@ -407,7 +402,7 @@ public class PartFilledApartmentListTest {
         createSingleElementCollection();
         fillWithData();
         final ListIterator<Apartment> apartmentListIterator = sut.listIterator();
-        assertFalse("Должен не найти предыдущий элемент", apartmentListIterator.hasPrevious());
+        assertFalse(apartmentListIterator.hasPrevious());
     }
 
     @Test
@@ -416,7 +411,7 @@ public class PartFilledApartmentListTest {
         fillWithData();
         final ListIterator<Apartment> apartmentListIterator = sut.listIterator();
         apartmentListIterator.next();
-        assertEquals("Должен найти предыдущий элемент", TEST_APARTMENTS.get(0), apartmentListIterator.previous());
+        assertEquals(TEST_APARTMENTS.get(0), apartmentListIterator.previous());
     }
 
     @Test
@@ -467,21 +462,21 @@ public class PartFilledApartmentListTest {
         }
         final List<Apartment> resultList = new ArrayList<>(TEST_APARTMENTS.subList(0, MAX_SIZE));
         resultList.set(4, null);
-        assertEquals("Должен удалить элемент из коллекции ", resultList, sut);
+        assertEquals(resultList, sut);
 
     }
 
     @Test
     public void shouldRemoveAllFromCollection() {
         fillWithData();
-        assertTrue("Должен удалить все данные из набора", sut.removeAll(ImmutableList.of(TEST_APARTMENTS.get(2), TEST_APARTMENTS.get(4))));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(0)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(1)));
-        assertFalse("Элемент должен быть удален", sut.contains(TEST_APARTMENTS.get(2)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(3)));
-        assertFalse("Элемент должен быть удален", sut.contains(TEST_APARTMENTS.get(4)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(5)));
-        assertFalse("Должен не удалить все данные из набора большего чем лист", sut.removeAll(TEST_APARTMENTS));
+        assertTrue(sut.removeAll(ImmutableList.of(TEST_APARTMENTS.get(2), TEST_APARTMENTS.get(4))));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(0)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(1)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(2)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(3)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(4)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(5)));
+        assertFalse(sut.removeAll(TEST_APARTMENTS));
     }
 
     @Test
@@ -492,31 +487,31 @@ public class PartFilledApartmentListTest {
         listForRemove.add(null);
         listForRemove.add(TEST_APARTMENTS.get(4));
         listForRemove.add(null);
-        assertTrue("Должен удалить все данные из набора", sut.removeAll(listForRemove));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(0)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(1)));
-        assertFalse("Элемент должен быть удален", sut.contains(TEST_APARTMENTS.get(2)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(3)));
-        assertFalse("Элемент должен быть удален", sut.contains(TEST_APARTMENTS.get(4)));
-        assertTrue("Элемент должен остаться", sut.contains(TEST_APARTMENTS.get(5)));
-        assertFalse("Должен не удалить все данные из набора большего чем лист", sut.removeAll(TEST_APARTMENTS));
+        assertTrue(sut.removeAll(listForRemove));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(0)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(1)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(2)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(3)));
+        assertFalse(sut.contains(TEST_APARTMENTS.get(4)));
+        assertTrue(sut.contains(TEST_APARTMENTS.get(5)));
+        assertFalse(sut.removeAll(TEST_APARTMENTS));
     }
 
     @Test
     public void shouldNotRemoveIfOnlyNulls() {
         fillWithData();
         final List<Apartment> oldList = new ArrayList<>(sut);
-        assertFalse("Должен не удалить все данные из набора null", sut.removeAll(Collections.singletonList(null)));
-        assertEquals("Должен не удалить все данные из набора null, данные не изменились", oldList, sut);
+        assertFalse(sut.removeAll(Collections.singletonList(null)));
+        assertEquals(oldList, sut);
     }
 
     @Test
     public void shouldClearAllData() {
         fillWithData();
         sut.clear();
-        assertEquals("Лист должен быть нулевой длинны после удаления", 0, sut.size());
+        assertEquals(0, sut.size());
         fillWithData();
-        assertEquals("Лист должен быть нулевой длинны после удаления (после добавления)", 0, sut.size());
+        assertEquals(0, sut.size());
     }
 
     @Test(expected = UnsupportedOperationException.class)
